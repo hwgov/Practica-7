1b3cf49f95fafdbbd49d3114630c8869e97cf29b
hwgov <hugovicenteortizmarin43@gmail.com>
2026-03-26 14:17:04 -0600

    Ejercicio5.c

--- Ejercicio5.c
+++ Ejercicio5.c
@@ -1,26 +1,26 @@
-#include <stdio.h>
 int main()
 {
-    int op;
+    char op = '\0';
     printf("\tMenú\n\n");
     printf("Elegir la opción deseada\n"); 
-    printf("1) Ingresar\n");
-    printf("2) Registrarse\n"); 
-    printf("3) Salir\n"); 
-    scanf("%d",&op);
-    switch(op) 
+    printf("a) Ingresar\n");
+    printf("b) Registrarse\n"); 
+    printf("c) Salir\n");
+    scanf("%c",&op);
+    switch(op)
     {
-        case 1:
-            printf("Se seleccionó 'Ingresar'\n"); 
-        break;
-        case 2:
-            printf("Se seleccionó 'Registrarse'\n"); 
-        break;
-        case 3:
-            printf("Se seleccionó 'Salir'\n");
-        break;
         default:
-            printf("Opción no válida\n");
+            printf("Opción no válida.\n");
+        break;
+        case 'a':
+            printf("Se seleccionó 'Ingresar'.\n");
+        break;
+        case 'b':
+            printf("Se seleccionó 'Registrarse'.\n"); 
+        break;
+        case 'c':
+            printf("Se seleccionó 'Salir'.\n");
+        break;
     }
     return 0;
 }
